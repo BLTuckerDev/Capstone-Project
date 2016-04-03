f95df0297182a34a31a5732b6b1d429538f6b8e9
Brett <bretttucker@gmail.com>
2016-04-03 09:14:14 -0600

    Limit story downloads
    I dont believe the normal use case for this app will require that we
    download all 500 of the top stories. I am limiting it to syncing only
    the top 150 stories which I believe should still be more than enough for
    all reasonable use cases.

--- app/src/main/java/dev/bltucker/nanodegreecapstone/sync/StorySyncAdapter.java
+++ app/src/main/java/dev/bltucker/nanodegreecapstone/sync/StorySyncAdapter.java
@@ -8,7 +8,6 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.SyncResult;
 import android.os.Bundle;
-import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,12 +19,8 @@ import dev.bltucker.nanodegreecapstone.data.CommentRefsColumns;
 import dev.bltucker.nanodegreecapstone.data.HackerNewsApiService;
 import dev.bltucker.nanodegreecapstone.data.SchematicContentProviderGenerator;
 import dev.bltucker.nanodegreecapstone.data.Story;
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
 import rx.Observable;
 import rx.Subscriber;
-import rx.functions.Action1;
 import rx.functions.Func1;
 import timber.log.Timber;
 
@@ -34,13 +29,14 @@ public final class StorySyncAdapter extends AbstractThreadedSyncAdapter {
     public static final String ACCOUNT = "storySyncAccount";
 
     public static final String ACCOUNT_TYPE = "bltucker.dev";
+    public static final int MAX_STORY_COUNT = 150;
 
     private final ContentResolver contentResolver;
 
     @Inject
     HackerNewsApiService apiService;
 
-    public StorySyncAdapter(Context context, boolean autoInitialize){
+    public StorySyncAdapter(Context context, boolean autoInitialize) {
         super(context, autoInitialize);
         contentResolver = context.getContentResolver();
         CapstoneApplication.getApplication().getApplicationComponent().inject(this);
@@ -57,61 +53,65 @@ public final class StorySyncAdapter extends AbstractThreadedSyncAdapter {
 
         Timber.d("Syncing....");
 
+        contentResolver.delete(SchematicContentProviderGenerator.StoryPaths.ALL_STORIES, null, null);
+        contentResolver.delete(SchematicContentProviderGenerator.CommentRefs.ALL_COMMENTS, null, null);
+
         apiService.getTopStoryIds()
-                .concatMap(new Func1<List<Long>, Observable<List<Observable<Story>>>>() {
-                    @Override
-                    public Observable<List<Observable<Story>>> call(List<Long> storyIds) {
-                        List<Observable<Story>> observableStoryList = new ArrayList<>(storyIds.size());
-                        for (int i = 0; i < storyIds.size(); i++) {
-                            observableStoryList.add(apiService.getStory(storyIds.get(i)));
-                        }
-                        return Observable.just(observableStoryList);
-                    }
-                })
-                .subscribe(new Subscriber<List<Observable<Story>>>() {
-                    @Override
-                    public void onCompleted() {
-                    }
+              .concatMap(new Func1<List<Long>, Observable<List<Observable<Story>>>>() {
+                  @Override
+                  public Observable<List<Observable<Story>>> call(List<Long> storyIds) {
+                      List<Observable<Story>> observableStoryList = new ArrayList<>(storyIds.size());
+                      final int maxStories = Math.min(storyIds.size(), MAX_STORY_COUNT);
 
-                    @Override
-                    public void onError(Throwable e) {
-                        Timber.e(e, "Error downloading top stories");
-                    }
+                      for (int i = 0; i < maxStories; i++) {
+                          observableStoryList.add(apiService.getStory(storyIds.get(i)));
+                      }
 
-                    @Override
-                    public void onNext(List<Observable<Story>> observables) {
-                        List<ContentValues> storyContentValues = new ArrayList<>(observables.size());
-                        List<ContentValues> commentRefsContentValuesList = new ArrayList<>();
+                      return Observable.just(observableStoryList);
+                  }
+              })
+              .subscribe(new Subscriber<List<Observable<Story>>>() {
+                  @Override
+                  public void onCompleted() {
+                  }
 
-                        for (int i = 0; i < observables.size(); i++) {
-                            Story story = observables.get(i).toBlocking().first();
-                            storyContentValues.add(Story.mapToContentValues(story));
-                            commentRefsContentValuesList.addAll(getCommentRefList(story));
-                        }
+                  @Override
+                  public void onError(Throwable e) {
+                      Timber.e(e, "Error downloading top stories");
+                  }
 
-                        ContentValues[] contentValues = new ContentValues[storyContentValues.size()];
-                        int insertedStoryCount = contentResolver.bulkInsert(SchematicContentProviderGenerator.StoryPaths.ALL_STORIES, storyContentValues.toArray(contentValues));
+                  @Override
+                  public void onNext(List<Observable<Story>> observables) {
+                      List<ContentValues> storyContentValues = new ArrayList<>(observables.size());
+                      List<ContentValues> commentRefsContentValuesList = new ArrayList<>();
 
-                        Timber.d("Inserted %d stories", insertedStoryCount);
+                      for (int i = 0; i < observables.size(); i++) {
+                          Story story = observables.get(i).toBlocking().first();
+                          storyContentValues.add(Story.mapToContentValues(story));
+                          commentRefsContentValuesList.addAll(getCommentRefList(story));
+                      }
 
-                        ContentValues[] commentRefsContentValuesArray = new ContentValues[commentRefsContentValuesList.size()];
-                        int commentRefInsertCount = contentResolver.bulkInsert(SchematicContentProviderGenerator.CommentRefs.ALL_COMMENTS, commentRefsContentValuesList.toArray(commentRefsContentValuesArray));
+                      ContentValues[] contentValues = new ContentValues[storyContentValues.size()];
+                      int insertedStoryCount = contentResolver.bulkInsert(SchematicContentProviderGenerator.StoryPaths.ALL_STORIES, storyContentValues.toArray(contentValues));
 
-                        Timber.d("Inserted %d comment references", commentRefInsertCount);
+                      Timber.d("Inserted %d stories", insertedStoryCount);
 
-                    }
-                });
+                      ContentValues[] commentRefsContentValuesArray = new ContentValues[commentRefsContentValuesList.size()];
+                      int commentRefInsertCount = contentResolver.bulkInsert(SchematicContentProviderGenerator.CommentRefs.ALL_COMMENTS, commentRefsContentValuesList.toArray(commentRefsContentValuesArray));
+
+                      Timber.d("Inserted %d comment references", commentRefInsertCount);
+                  }
+              });
     }
 
 
-
-    private List<ContentValues> getCommentRefList(Story story){
+    private List<ContentValues> getCommentRefList(Story story) {
         long[] commentIds = story.getCommentIds();
         List<ContentValues> contentValues = new ArrayList<>(commentIds.length);
 
         for (int i = 0; i < commentIds.length; i++) {
             ContentValues aContentValue = new ContentValues();
-            aContentValue.put(CommentRefsColumns._ID,  commentIds[i]);
+            aContentValue.put(CommentRefsColumns._ID, commentIds[i]);
             aContentValue.put(CommentRefsColumns.STORY_ID, story.getId());
             contentValues.add(aContentValue);
         }
