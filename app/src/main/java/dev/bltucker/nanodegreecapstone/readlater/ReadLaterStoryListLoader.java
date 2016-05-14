12d02445b0ca30716736f68d0cb57d5f4b5d9f87
Brett <bretttucker@gmail.com>
2016-05-14 10:46:11 -0600

    Add loader for read later stories

---
+++ app/src/main/java/dev/bltucker/nanodegreecapstone/readlater/ReadLaterStoryListLoader.java
@@ -0,0 +1,36 @@
+package dev.bltucker.nanodegreecapstone.readlater;
+
+import android.content.Context;
+import android.support.v4.content.AsyncTaskLoader;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import dev.bltucker.nanodegreecapstone.models.ReadLaterStory;
+
+public class ReadLaterStoryListLoader extends AsyncTaskLoader<List<ReadLaterStory>> {
+
+    public static final int ID = ReadLaterStoryListLoader.class.hashCode();
+
+    private final ReadLaterRepository readLaterRepository;
+
+    @Inject
+    public ReadLaterStoryListLoader(Context context, ReadLaterRepository readLaterRepository){
+        super(context);
+        this.readLaterRepository = readLaterRepository;
+        onContentChanged();
+    }
+
+    @Override
+    public List<ReadLaterStory> loadInBackground() {
+        return readLaterRepository.getAll().toBlocking().first();
+    }
+
+    @Override
+    protected void onStartLoading() {
+        if (takeContentChanged()) {
+            forceLoad();
+        }
+    }
+}
