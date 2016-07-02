9f3feb8f492a90319997fdbd2a38c16815babfc3
Brett <bretttucker@gmail.com>
2016-07-01 19:38:29 -0600

    Add items necessary to inject CommentRepository

--- app/src/main/java/dev/bltucker/nanodegreecapstone/injection/ApplicationResourcesModule.java
+++ app/src/main/java/dev/bltucker/nanodegreecapstone/injection/ApplicationResourcesModule.java
@@ -5,6 +5,7 @@ import android.app.NotificationManager;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Resources;
+import android.util.LruCache;
 
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.Tracker;
@@ -12,6 +13,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.util.Calendar;
+import java.util.List;
 import java.util.TimeZone;
 
 import dagger.Module;
@@ -37,79 +39,92 @@ public class ApplicationResourcesModule {
 
     private final CapstoneApplication application;
 
-    public ApplicationResourcesModule(CapstoneApplication application){
+    public ApplicationResourcesModule(CapstoneApplication application) {
         this.application = application;
     }
 
     @Provides
     @ApplicationScope
-    public ContentResolver provideContentResolver(){
+    public ContentResolver provideContentResolver() {
         return application.getContentResolver();
     }
 
     @Provides
     @ApplicationScope
-    public NotificationManager provideNotificationManager(){
+    public NotificationManager provideNotificationManager() {
         return (NotificationManager) application.getSystemService(Context.NOTIFICATION_SERVICE);
     }
 
     @Provides
     @ApplicationScope
-    public Tracker provideTracker(){
+    public Tracker provideTracker() {
         return GoogleAnalytics.getInstance(application).newTracker(R.xml.global_tracker);
     }
 
     @Provides
     @ApplicationScope
-    public Resources provideResources(Context context){
+    public Resources provideResources(Context context) {
         return context.getResources();
     }
 
     @Provides
     @ApplicationScope
     @StoryMax
-    public int provideMaxStoryInt(Context context){
-        return context.getResources().getInteger(R.integer.reading_session_story_max);
+    public int provideMaxStoryInt(Resources resources) {
+        return resources.getInteger(R.integer.reading_session_story_max);
     }
 
     @Provides
     @ApplicationScope
     @SyncIntervalSeconds
-    public int provideSyncIntervalInSeconds(Context context){
-        return context.getResources().getInteger(R.integer.sync_interval_seconds);
+    public int provideSyncIntervalInSeconds(Resources resources) {
+        return resources.getInteger(R.integer.sync_interval_seconds);
     }
 
     @Provides
     @ApplicationScope
-    public Account provideAccount(){
+    @CommentCacheSize
+    public int provideCommentCacheSize(Resources resources){
+        return resources.getInteger(R.integer.comment_cache_size);
+    }
+
+    @Provides
+    @ApplicationScope
+    public Account provideAccount() {
         return new Account(StorySyncAdapter.ACCOUNT, StorySyncAdapter.ACCOUNT_TYPE);
     }
 
     @Provides
     @ApplicationScope
     @GregorianUTC
-    public Calendar provideCalendar(){
+    public Calendar provideCalendar() {
         return Calendar.getInstance(TimeZone.getTimeZone("UTC"));
     }
 
     @Provides
     @ApplicationScope
-    public Gson provideGson(){
+    @CommentListCache
+    public LruCache<Long, List<Comment>> provideLruCache(@CommentCacheSize int cacheSize){
+        return new LruCache<>(cacheSize);
+    }
+
+    @Provides
+    @ApplicationScope
+    public Gson provideGson() {
         GsonBuilder builder = new GsonBuilder();
         builder.registerTypeAdapter(Comment.class, new CommentTypeAdapter());
         return builder.create();
     }
 
-
     @Provides
     @ApplicationScope
-    public Context provideApplicationContext(){
+    public Context provideApplicationContext() {
         return application;
     }
 
     @Provides
     @ApplicationScope
-    public Retrofit provideRetrofitClient(Gson gson){
+    public Retrofit provideRetrofitClient(Gson gson) {
         return new Retrofit.Builder()
                 .baseUrl("https://hacker-news.firebaseio.com/v0/")
                 .addConverterFactory(GsonConverterFactory.create(gson))
@@ -119,33 +134,31 @@ public class ApplicationResourcesModule {
 
     @Provides
     @ApplicationScope
-    public HackerNewsApiService provideApiService(Retrofit retrofit){
+    public HackerNewsApiService provideApiService(Retrofit retrofit) {
         return retrofit.create(HackerNewsApiService.class);
     }
 
     @Provides
     @ApplicationScope
-    public StoryRepository provideStoryRepository(ContentResolver contentResolver, CommentRepository commentRepository){
+    public StoryRepository provideStoryRepository(ContentResolver contentResolver, CommentRepository commentRepository) {
         return new ContentProviderBackedStoryRepository(contentResolver, commentRepository);
     }
 
     @Provides
     @ApplicationScope
-    public CommentRepository provideCommentRepository(HackerNewsApiService hackerNewsApiService, ContentResolver contentResolver){
-        return new CommentRepository(hackerNewsApiService, contentResolver);
+    public CommentRepository provideCommentRepository(HackerNewsApiService hackerNewsApiService, ContentResolver contentResolver, @CommentListCache LruCache<Long, List<Comment>> cache) {
+        return new CommentRepository(hackerNewsApiService, contentResolver, cache);
     }
 
     @Provides
     @ApplicationScope
-    public EventBus provideEventBus(){
+    public EventBus provideEventBus() {
         return new EventBus();
     }
 
-
-
     @Provides
     @ApplicationScope
-    public ReadingSession provideReadingSession(@StoryMax int storyMax, EventBus eventBus){
+    public ReadingSession provideReadingSession(@StoryMax int storyMax, EventBus eventBus) {
         return new ReadingSession(storyMax, eventBus);
     }
 }
