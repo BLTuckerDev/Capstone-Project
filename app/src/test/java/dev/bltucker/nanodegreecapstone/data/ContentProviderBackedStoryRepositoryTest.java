c907b21cb691785ced0743f17f2cbd3dcd6b596d
Brett <bretttucker@gmail.com>
2016-07-01 22:02:05 -0600

    Add more tests

--- app/src/test/java/dev/bltucker/nanodegreecapstone/data/ContentProviderBackedStoryRepositoryTest.java
+++ app/src/test/java/dev/bltucker/nanodegreecapstone/data/ContentProviderBackedStoryRepositoryTest.java
@@ -1,6 +1,7 @@
 package dev.bltucker.nanodegreecapstone.data;
 
 import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
 
@@ -8,23 +9,29 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.modules.junit4.PowerMockRunnerDelegate;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import dev.bltucker.nanodegreecapstone.models.Story;
 
 import static junit.framework.Assert.assertEquals;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ContentResolver.class, Uri.class})
+@PrepareForTest({ContentResolver.class, Uri.class, ContentValues.class, Story.class, ContentProviderBackedStoryRepository.class})
 @PowerMockRunnerDelegate(MockitoJUnitRunner.class)
 public class ContentProviderBackedStoryRepositoryTest {
 
@@ -95,6 +102,33 @@ public class ContentProviderBackedStoryRepositoryTest {
 
     @Test
     public void testSaveStories() throws Exception {
+        Uri mockUri = mock(Uri.class);
+        ContentValues mockContentValues = mock(ContentValues.class);
+
+        mockStatic(Uri.class);
+        mockStatic(Story.class);
+        mockStatic(ContentValues.class);
+
+        when(Uri.parse(anyString())).thenReturn(mockUri);
+        when(mockContentResolver.delete(mockUri, null, null)).thenReturn(1);
+
+        Long[] commentIds = {1L, 2L, 3L};
+        Story fakeStory = new Story(1L, "Poster", 100, new Date().getTime(), "Post Title", "https://google.com/", commentIds);
+
+        List<Story> stories = new ArrayList<Story>();
+        stories.add(fakeStory);
+
+        when(Story.mapToContentValues(fakeStory)).thenReturn(mockContentValues);
+        PowerMockito.whenNew(ContentValues.class).withAnyArguments().thenReturn(mockContentValues);
+        PowerMockito.doNothing().when(mockContentValues).put(CommentRefsColumns._ID, 1L);
+        doNothing().when(mockContentValues).put(CommentRefsColumns._ID, 1L);
+
+
+        objectUnderTest.saveStories(stories);
+
+        verify(mockCommentRepository).clearInMemoryCache();
+        verify(mockContentResolver, times(2)).delete(any(Uri.class), anyString(), any(String[].class));
+        verify(mockContentResolver, times(2)).bulkInsert(any(Uri.class), any(ContentValues[].class));
 
     }
 }
