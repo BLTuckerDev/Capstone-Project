4718a6e2269a8a853820268b655cf54266d0a533
Brett <bretttucker@gmail.com>
2016-09-20 20:53:50 -0600

    Add schematic columns interface for comments

---
+++ app/src/main/java/dev/bltucker/nanodegreecapstone/storydetail/data/CommentColumns.java
@@ -0,0 +1,27 @@
+package dev.bltucker.nanodegreecapstone.storydetail.data;
+
+import net.simonvt.schematic.annotation.ConflictResolutionType;
+import net.simonvt.schematic.annotation.DataType;
+import net.simonvt.schematic.annotation.PrimaryKey;
+
+public interface CommentColumns {
+
+    @DataType(DataType.Type.INTEGER) @PrimaryKey(onConflict = ConflictResolutionType.REPLACE)
+    String _ID = "_id";
+
+    @DataType(DataType.Type.INTEGER)
+    String COMMENT_ID = "commentId";
+
+    @DataType(DataType.Type.TEXT)
+    String AUTHOR_NAME = "authorName";
+
+    @DataType(DataType.Type.TEXT)
+    String COMMENT_TEXT = "commentText";
+
+    @DataType(DataType.Type.INTEGER)
+    String UNIX_POST_TIME = "unixPostTime";
+
+    @DataType(DataType.Type.INTEGER)
+    String PARENT_ID = "parentId";
+
+}
