e1246cdb9b47a3fe0d58801737f16cdc88a737f1
Brett <bretttucker@gmail.com>
2016-09-29 20:38:59 -0600

    Listen to comment sync complete events
    The comment loader will now listen for the comment service's sync
    complete event so that it knows when to reload additional comments.

--- app/src/main/java/dev/bltucker/nanodegreecapstone/storydetail/StoryCommentsLoader.java
+++ app/src/main/java/dev/bltucker/nanodegreecapstone/storydetail/StoryCommentsLoader.java
@@ -8,8 +8,12 @@ import java.util.List;
 import javax.inject.Inject;
 
 import dev.bltucker.nanodegreecapstone.data.CommentRepository;
+import dev.bltucker.nanodegreecapstone.events.EventBus;
+import dev.bltucker.nanodegreecapstone.events.StoryCommentsDownloadCompleteEvent;
 import dev.bltucker.nanodegreecapstone.models.Comment;
 import rx.Subscriber;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 import timber.log.Timber;
 
@@ -20,12 +24,15 @@ public class StoryCommentsLoader extends AsyncTaskLoader<Void> {
     public static final int STORY_COMMENT_LOADER = StoryCommentsLoader.class.hashCode();
 
     private final CommentRepository commentRepository;
+    private final EventBus eventBus;
     private DetailStory detailStory;
+    private Subscription syncCompletedSubscription;
 
     @Inject
-    public StoryCommentsLoader(Context context, CommentRepository repository){
+    public StoryCommentsLoader(Context context, CommentRepository repository, EventBus eventBus){
         super(context);
         commentRepository = repository;
+        this.eventBus = eventBus;
         onContentChanged();
     }
 
@@ -58,9 +65,40 @@ public class StoryCommentsLoader extends AsyncTaskLoader<Void> {
 
     @Override
     protected void onStartLoading() {
+        subscribeToSyncCompleteEvent();
+
         if (takeContentChanged()) {
             forceLoad();
         }
+    }
+
+    @Override
+    protected void onReset() {
+        syncCompletedSubscription.unsubscribe();
+        super.onReset();
+    }
+
+    private void subscribeToSyncCompleteEvent() {
+        //TODO pay attention to what story we are downloading here
+        syncCompletedSubscription = eventBus.subscribeTo(StoryCommentsDownloadCompleteEvent.class)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Object>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Timber.e(e, "Error while listening for comment updates");
+                    }
+
+                    @Override
+                    public void onNext(Object o) {
+                        Timber.d("Story Comment sync completed! Loader will now perform onContentChanged");
+                        onContentChanged();
+                    }
+                });
     }
 
     public void setDetailStory(DetailStory detailStory) {
