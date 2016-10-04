788c2b48a4ad0882694668f5ee088c0e077cd8c2
Brett <bretttucker@gmail.com>
2016-10-03 19:57:59 -0600

    Add UI test to verify that we show the story list

--- app/src/androidTest/java/dev/bltucker/nanodegreecapstone/home/MainActivityTest.java
+++ app/src/androidTest/java/dev/bltucker/nanodegreecapstone/home/MainActivityTest.java
@@ -1,6 +1,7 @@
 package dev.bltucker.nanodegreecapstone.home;
 
 import android.content.Context;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
@@ -23,6 +24,7 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
@@ -53,15 +55,11 @@ public class MainActivityTest {
 
     @Test
     public void testMainActivity(){
-
         activityRule.launchActivity(null);
 
-        HomeViewPresenter homeViewPresenter = ((MainActivity) activityRule.getActivity()).homeViewPresenter;
+        onView(withText(activityRule.getActivity().getString(R.string.app_name))).check(matches(isDisplayed()));
 
-        onView(withId(R.id.content_container)).check(matches(isDisplayed()));
-
-
-
+        onView(withId(R.id.story_list_recyclerview)).check(matches(isDisplayed()));
 
     }
 
