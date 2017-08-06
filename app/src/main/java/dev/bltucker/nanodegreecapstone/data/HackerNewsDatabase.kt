5e085d25af00f6abb3740fc64521986f37b34cae
Brett <bretttucker@gmail.com>
2017-08-06 15:05:33 -0600

    Use kapt in addition to annotationProcessor

---
+++ app/src/main/java/dev/bltucker/nanodegreecapstone/data/HackerNewsDatabase.kt
@@ -0,0 +1,11 @@
+package dev.bltucker.nanodegreecapstone.data
+
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.RoomDatabase
+
+import dev.bltucker.nanodegreecapstone.models.Comment
+import dev.bltucker.nanodegreecapstone.models.ReadLaterStory
+import dev.bltucker.nanodegreecapstone.models.Story
+
+@Database(entities = arrayOf(Story::class, CommentReference::class, ReadLaterStory::class, Comment::class), version = 2)
+abstract class HackerNewsDatabase : RoomDatabase()
