364ad9d1453d073757c0b08dc13d84251ad456a8
Brett <bretttucker@gmail.com>
2017-10-07 17:19:37 -0600

    Convert CommentRepository to Kotlin

---
+++ app/src/main/java/dev/bltucker/nanodegreecapstone/storydetail/CommentRepository.kt
@@ -0,0 +1,32 @@
+package dev.bltucker.nanodegreecapstone.storydetail
+
+import javax.inject.Inject
+
+import dev.bltucker.nanodegreecapstone.data.HackerNewsApiService
+import dev.bltucker.nanodegreecapstone.data.daos.CommentsDao
+import dev.bltucker.nanodegreecapstone.injection.ApplicationScope
+import dev.bltucker.nanodegreecapstone.models.Comment
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+
+@ApplicationScope
+class CommentRepository @Inject
+constructor(private val commentsDao: CommentsDao) {
+
+    fun getCommentsForStoryId(storyId: Long): Observable<Array<Comment>> {
+        return getLocalComments(storyId)
+    }
+
+    fun saveComment(comment: Comment) {
+        commentsDao.save(comment)
+    }
+
+    private fun getLocalComments(storyId: Long): Observable<Array<Comment>> {
+        return commentsDao.getStoryCommentsFlowable(storyId)
+                .onBackpressureDrop()
+                .toObservable()
+    }
+
+}
