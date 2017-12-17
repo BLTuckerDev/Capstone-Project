74e18e7ed53f9367f228b8130ddf5dd07f0aa162
Brett <bretttucker@gmail.com>
2017-12-17 16:38:44 -0700

    Convert StoryListAdapter to Kotlin

---
+++ app/src/main/java/dev/bltucker/nanodegreecapstone/topstories/StoryListAdapter.kt
@@ -0,0 +1,74 @@
+package dev.bltucker.nanodegreecapstone.topstories
+
+import android.support.v7.util.DiffUtil
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dev.bltucker.nanodegreecapstone.R
+import dev.bltucker.nanodegreecapstone.common.injection.ApplicationScope
+import dev.bltucker.nanodegreecapstone.databinding.HeadlineItemLayoutBinding
+import dev.bltucker.nanodegreecapstone.models.Story
+import java.util.*
+import javax.inject.Inject
+
+@ApplicationScope
+class StoryListAdapter @Inject
+internal constructor(private val topStoriesViewModel: TopStoriesViewModel) : RecyclerView.Adapter<StoryListAdapter.StoryHeadlineViewHolder>() {
+
+    private val stories: MutableList<Story>
+
+    init {
+        stories = ArrayList()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StoryListAdapter.StoryHeadlineViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = HeadlineItemLayoutBinding.inflate(inflater, parent, false)
+        return StoryHeadlineViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: StoryListAdapter.StoryHeadlineViewHolder, position: Int) {
+        val story = stories[position]
+        holder.bindStory(story)
+    }
+
+    override fun getItemCount(): Int {
+        return stories.size
+    }
+
+    fun updateStories(updatedStories: List<Story>) {
+
+        val diffResult = DiffUtil.calculateDiff(StoryDiffUtilCallback(this.stories, updatedStories))
+
+        this.stories.clear()
+        this.stories.addAll(updatedStories)
+        diffResult.dispatchUpdatesTo(this)
+    }
+
+
+    inner class StoryHeadlineViewHolder(private val binding: HeadlineItemLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        lateinit var story: Story
+
+        init {
+            binding.headlineCardView.setOnClickListener { v -> topStoriesViewModel.onCommentsButtonClick(story) }
+            binding.commentsButton.setOnClickListener { v -> topStoriesViewModel.onCommentsButtonClick(story) }
+            binding.readButton.setOnClickListener { v -> topStoriesViewModel.onReadLaterStoryButtonClick(story) }
+        }
+
+        fun bindStory(story: Story) {
+            this.story = story
+            val viewContext = itemView.context
+            binding.storyTitleTextview.text = story.getTitle()
+            binding.posterNameTextview.text = String.format(viewContext.getString(R.string.by_poster), story.getPosterName())
+            binding.scoreTextview.text = story.getScore().toString()
+            if (null == story.getUrl()) {
+                binding.readButton.visibility = View.INVISIBLE
+            } else {
+                binding.readButton.visibility = View.VISIBLE
+            }
+        }
+    }
+
+}
