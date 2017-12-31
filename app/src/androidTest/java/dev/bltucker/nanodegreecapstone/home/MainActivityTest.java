1fe255f953513cfcfa499321af8c33b972d71cee
Brett <bretttucker@gmail.com>
2017-12-30 19:04:34 -0700

    Remove unused code

--- app/src/androidTest/java/dev/bltucker/nanodegreecapstone/home/MainActivityTest.java
+++ app/src/androidTest/java/dev/bltucker/nanodegreecapstone/home/MainActivityTest.java
@@ -1,8 +1,6 @@
 package dev.bltucker.nanodegreecapstone.home;
 
 import android.content.Context;
-import android.support.test.espresso.Espresso;
-import android.support.test.espresso.IdlingResource;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -14,14 +12,10 @@ import org.junit.runner.RunWith;
 
 import dev.bltucker.nanodegreecapstone.CapstoneApplication;
 import dev.bltucker.nanodegreecapstone.R;
-import dev.bltucker.nanodegreecapstone.events.EventBus;
-import dev.bltucker.nanodegreecapstone.events.SyncCompletedEvent;
 import dev.bltucker.nanodegreecapstone.common.injection.ApplicationComponent;
 import dev.bltucker.nanodegreecapstone.common.injection.DaggerApplicationComponent;
 import dev.bltucker.nanodegreecapstone.common.injection.DaggerInjector;
 import dev.bltucker.nanodegreecapstone.common.injection.TestApplicationResourcesModule;
-import io.reactivex.Observer;
-import io.reactivex.disposables.Disposable;
 import timber.log.Timber;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
@@ -50,31 +44,6 @@ public class MainActivityTest {
                 .build();
 
         DaggerInjector.initializeInjector(applicationComponent);
-
-        EventBus eventBus = DaggerInjector.getApplicationComponent().eventBus();
-
-        Timber.d("Registered idling resource");
-        eventBus.subscribeTo(SyncCompletedEvent.class)
-                .subscribe(new Observer<Object>() {
-                    @Override
-                    public void onComplete() {
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                    }
-
-                    @Override
-                    public void onSubscribe(Disposable d) {
-
-                    }
-
-                    @Override
-                    public void onNext(Object o) {
-                        Timber.d("Resource is now idle");
-                    }
-                });
-
     }
 
     @After
@@ -91,40 +60,4 @@ public class MainActivityTest {
         onView(withId(R.id.story_list_recyclerview)).check(matches(isDisplayed()));
 
     }
-
-
-    public class SyncAdapterIdlingResource implements IdlingResource {
-
-        private ResourceCallback resourceCallback;
-        private volatile boolean isIdle;
-
-        public SyncAdapterIdlingResource() {
-            isIdle = false;
-        }
-
-
-        @Override
-        public String getName() {
-            return this.getClass().getName();
-        }
-
-        @Override
-        public boolean isIdleNow() {
-            if (isIdle && resourceCallback != null) {
-                resourceCallback.onTransitionToIdle();
-            }
-            return isIdle;
-        }
-
-        @Override
-        public void registerIdleTransitionCallback(ResourceCallback callback) {
-            this.resourceCallback = callback;
-        }
-
-
-        public synchronized void setIdle(boolean idle) {
-            isIdle = idle;
-        }
-    }
-
 }
